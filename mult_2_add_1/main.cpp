d1f7dd3a9ce6282c69396bbff04b9698f54d77b2
StrahilRuychev <strahil.ruychev@gmail.com>
2024-02-13 15:40:29 +0200

    Initial Commit

---
+++ mult_2_add_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mult_2_add_1
+//
+//  Created by Strahil Ruychev on 13.02.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
