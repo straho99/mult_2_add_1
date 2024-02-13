8b757895fbe53b0485a7713710270b765f6a0259
StrahilRuychev <strahil.ruychev@gmail.com>
2024-02-13 17:15:18 +0200

    Full solution

--- mult_2_add_1/main.cpp
+++ mult_2_add_1/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  mult_2_add_1
-//
-//  Created by Strahil Ruychev on 13.02.24.
-//
-
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int n;
+    cin >> n;
+    
+    int k = 1;
+    
+    while(pow(2, k) <= n) {
+        k+=1;
+    }
+    
+    cout << k << endl;
+    
+    int steps[k], i = k-1;
+
+    while (n != 0) {
+        if (n % 2 == 0) {
+            steps[i] = 1;
+        } else {
+            steps[i] = 2;
+        }
+        
+        n/=2;
+        i--;
+    }
+    
+    for (int j = 0; j < k; j++) {
+        if (j == 0) {
+            cout << steps[j];
+        } else {
+            cout << " " << steps[j];
+        }
+    }
+    cout << endl;
+    
     return 0;
 }
